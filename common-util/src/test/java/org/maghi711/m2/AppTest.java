b6e48e9f023969279596cad2e5c0f689a03c041e
Mahesh Jyothi Ramalingam <mahesh.jyothi.ramalingam@signify.com>
2025-07-20 10:20:42 +0530

    added multi module project support.

---
+++ common-util/src/test/java/org/maghi711/m2/AppTest.java
@@ -0,0 +1,19 @@
+package org.maghi711.m2;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
